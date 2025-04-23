e19e4c5d9456330b9b3264e53317be9b1167331c
Serhii Yehorov <syehorov@unixzone.org.ua>
2025-04-23 22:53:33 +0300

    Just empty lines cleanup

--- src/config.cpp
+++ src/config.cpp
@@ -128,9 +128,7 @@ static int parse_outputs(libconfig::Setting& outs, channel_t* channel, int i, in
             fdata->max_rec_length = outs[o].exists("max_rec_length") ? (double)(outs[o]["max_rec_length"]) : 3600.0;
             fdata->max_idle_length = outs[o].exists("max_idle_length") ? (double)(outs[o]["max_idle_length"]) : 0.5;
             fdata->delete_short_records = outs[o].exists("delete_short_records") ? (bool)(outs[o]["delete_short_records"]) : false;
-
             channel->outputs[oo].has_mp3_output = true;
-
 
             if (fdata->split_on_transmission) {
                 if (parsing_mixers) {
@@ -163,7 +161,6 @@ static int parse_outputs(libconfig::Setting& outs, channel_t* channel, int i, in
             fdata->dated_subdirectories = outs[o].exists("dated_subdirectories") ? (bool)(outs[o]["dated_subdirectories"]) : false;
             fdata->dated_subdir_format = outs[o].exists("dated_subdir_format") ? (outs[o]["dated_subdir_format"]) : "%d-%m-%y";
             fdata->suffix = ".cf32";
-
             fdata->continuous = outs[o].exists("continuous") ? (bool)(outs[o]["continuous"]) : false;
             fdata->append = (!outs[o].exists("append")) || (bool)(outs[o]["append"]);
             fdata->split_on_transmission = outs[o].exists("split_on_transmission") ? (bool)(outs[o]["split_on_transmission"]) : false;
